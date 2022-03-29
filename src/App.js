bbe585625d3f2fa16a1bd1d250e1deaffb78dca2
icecrimu <charles.banigon@gmail.com>
2022-03-29 23:48:10 +0800

    Add initial component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import "./App.css"
+import { Container, Stack, Button } from "react-bootstrap"
 
 function App() {
-  return <div className="App">App</div>
+  return (
+    <Container className="my-4">
+      <Stack direction="horizontal" gap={2} className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr)",
+          gap: "1rem",
+          alignItems: "flex-start"
+        }}
+      >
+        {/* BudgetCard component */}
+      </div>
+    </Container>
+  )
 }
 
 export default App
