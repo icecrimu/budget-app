8cdf7dca198a5eeb07edab2ade7fcd839484ee11
icecrimu <charles.banigon@gmail.com>
2022-03-30 11:07:10 +0800

    Add ProgressBar in BudgetCard component

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -14,8 +14,21 @@ export default function BudgetCard({ name, amount, max }) {
             </span>
           </div>
         </Card.Title>
-        <ProgressBar className="rounded-pill" variant="" />
+        <ProgressBar
+          className="rounded-pill"
+          variant={getProgressBarVariant(amount, max)}
+          min={0}
+          max={max}
+          now={amount}
+        />
       </Card.Body>
     </Card>
   )
+}
+
+function getProgressBarVariant(amount, max) {
+  const ratio = amount / max
+  if (ratio < 0.5) return "primary"
+  if (ratio < 0.75) return "warning"
+  return "danger"
 }
